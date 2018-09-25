68d61c69a86604b2fcbda5a036d71719c9d0ccf7
mektievp <mektievp@gmail.com>
2018-09-24 20:36:01 -0400

    Added Integration Tests to verify that unactivated accounts do not show up on the users index

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users :michael
     @non_admin = users :archer
+    @unactivated = users :malory
   end
 
   test "index including pagination" do
@@ -13,7 +14,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
     User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
+      end
     end
   end
 
@@ -24,9 +29,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination', count: 2
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
       end
     end
     assert_difference 'User.count', -1 do
@@ -38,5 +47,25 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as @non_admin
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "unactivated users should not show up in users index" do
+    log_in_as @non_admin
+    get users_path
+    User.paginate(page: 1).each do |user|
+      if !user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name
+      end
+    end
+  end
+
+  test "visiting a user that has not been activated should redirect to root url" do
+    log_in_as @non_admin
+    get user_path @unactivated
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'static_pages/home'
   end
 end
